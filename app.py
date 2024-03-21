a19ab72993dd7f3e323d41d02af928a2d9fd4a7e
MrPrinceHz <u21cse1065@cit.ac.in>
2024-03-22 00:40:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+import chainlit as cl
+from langchain import HuggingFaceHub,PromptTemplate, LLMChain
+#from langchain.schema import StrOutputParser
+#from langchain.prompts import ChatPromptTemplate
+
+os.environ['API_KEY']="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"
+
+model_id = 'google/gemma-7b-it'
+falcon_llm = HuggingFaceHub(huggingfacehub_api_token=os.environ['API_KEY'],
+                            repo_id=model_id,
+                            model_kwargs={"temperature":0.8,"max_new_tokens":300})
+
+template = """
+
+    human:{question}
+    Answer :
+    
+    """
+
+
+
+#prompt = PromptTemplate(template=template, input_variables=['question'])
+
+
+
+@cl.on_chat_start
+def on_chat_start():
+    
+    prompt = PromptTemplate(template=template, input_variables=['question'])
+
+    llm_chain = LLMChain(prompt=prompt, llm=falcon_llm,verbose=True) 
+    
+    cl.user_session.set("llm_chain",llm_chain)
+    
+@cl.on_message
+async def on_message(message:cl.Message):
+    llm_chain=cl.user_session.get("llm_chain")
+    
+    res = await llm_chain.arun(question=message.content,callbacks=[cl.AsyncLangchainCallbackHandler()])
+    
+    await cl.Message(content=res).send()
---
+++ chainlit.md
@@ -0,0 +1,5 @@
+# Welcome to AI AyurDost
+
+AI AyurDost
+    Your own AI Ayurvedic Doctor
+    Ask me anything about Ayurveda
